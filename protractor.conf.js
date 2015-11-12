280d415720940d08a457dc4f26d9ac3e2860f1af
fediltest0 <fed.il.test0@gmail.com>
2015-11-12 10:00:15 +0000

    chore(protractor) add name and build

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,7 +3,9 @@ exports.config = {
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   capabilities: {
     'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'name': 'test0 E2E',
+    'build': process.env.TRAVIS_BUILD_NUMBER
   },
   framework: 'jasmine',
   specs: ['test/spec.js'],
