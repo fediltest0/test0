bbdfe8034b5be29b3b02a80db617daf85e6544da
fediltest0 <fed.il.test0@gmail.com>
2015-11-12 08:25:43 +0000

    core(protractor) fix URL

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   framework: 'jasmine2',
   //seleniumAddress: 'http://localhost:4444/wd/hub',
-  seleniumAddress: 'http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'http://ondemand.saucelabs.com/wd/hub',
+  seleniumAddress: 'http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'ondemand.saucelabs.com/wd/hub',
   specs: ['test/spec.js']
 };
