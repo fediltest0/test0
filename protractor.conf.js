21f953956dbe377923d56f195de40796eb6e7714
fediltest0 <fed.il.test0@gmail.com>
2015-11-12 08:22:32 +0000

    chore(protractor) pass souce labs credentials ad part of selenium url

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,6 @@
 exports.config = {
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
   framework: 'jasmine2',
   //seleniumAddress: 'http://localhost:4444/wd/hub',
-  seleniumAddress: 'http://ondemand.saucelabs.com/wd/hub',
+  seleniumAddress: 'http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'http://ondemand.saucelabs.com/wd/hub',
   specs: ['test/spec.js']
 };
