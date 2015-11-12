f02730c4a4e90417346f7b3e5c6633c05cd19667
fediltest0 <fed.il.test0@gmail.com>
2015-11-12 07:59:45 +0000

    chore(protractor) fix selenium address

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   framework: 'jasmine2',
   //seleniumAddress: 'http://localhost:4444/wd/hub',
-  seleniumAddress: 'http://hub.browserstack.com/wd/hub',
+  seleniumAddress: 'http://ondemand.saucelabs.com/wd/hub',
   specs: ['test/spec.js']
 };
